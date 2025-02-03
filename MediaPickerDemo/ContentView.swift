3ff707b1795586d5a94fe81a36c454748012d600
taise-hikawa <cmmkarubi@gmail.com>
2025-02-03 19:17:30 +0900

    create demo proj

---
+++ MediaPickerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediaPickerDemo
+//
+//  Created by 樋川大聖 on 2025/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediaPickerDemo/MediaPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPickerDemoApp.swift
+//  MediaPickerDemo
+//
+//  Created by 樋川大聖 on 2025/02/03.
+//
+
+import SwiftUI
+
+@main
+struct MediaPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
