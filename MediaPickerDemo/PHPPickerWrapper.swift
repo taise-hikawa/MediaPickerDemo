c4e2b4a61549a54e3fa93d51c5d1759b56015e6b
taise-hikawa <cmmkarubi@gmail.com>
2025-02-03 19:18:35 +0900

    add PHPPickerWrapper

---
+++ MediaPickerDemo/PHPPickerWrapper.swift
@@ -0,0 +1,50 @@
+//
+//  PHPPickerWrapper 2.swift
+//  MediaPickerDemo
+//
+//  Created by 樋川大聖 on 2025/02/03.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PHPPickerWrapper: UIViewControllerRepresentable {
+    @Environment(\.dismiss) private var dismiss
+    let selectionLimit: Int
+    let filter: PHPickerFilter?
+    let didPick: ([PHPickerResult]) -> Void
+
+    init(selectionLimit: Int, filter: PHPickerFilter? = nil, didPick: @escaping ([PHPickerResult]) -> Void) {
+        self.selectionLimit = selectionLimit
+        self.filter = filter
+        self.didPick = didPick
+    }
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        if let filter {
+            configuration.filter = filter
+        }
+        configuration.selectionLimit = selectionLimit
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        private let parent: PHPPickerWrapper
+        init(parent: PHPPickerWrapper) {
+            self.parent = parent
+        }
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.dismiss()
+            parent.didPick(results)
+        }
+    }
+}
